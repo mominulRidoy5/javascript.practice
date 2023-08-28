5b7ae02b99d4a82951a8b38e3e4d2956a297d82a
Your Name <nusratriya887@gmail.com>
2023-08-28 14:14:48 +0600

    factory func

--- chapter-towelve/index.js
+++ chapter-towelve/index.js
@@ -4,12 +4,85 @@ var rect = {
 
     draw: function() {
         console.log('I am a rectAngle')
+    this.printProperties()
+    console.log(this)
+    },
+    printProperties: function() {
         console.log('My width is' + ' ' + this.width)
         console.log('My width is' + ' ' + this.height)
+
+    
     }
 }
 
 rect.draw()
 
-rect.height = 120
-rect.draw()
+// rect.height = 120
+// rect.draw()
+
+function myFunc () {
+    console.log(this)
+}
+
+myFunc()
+
+var another = {
+    width: 79,
+    height: 87,
+    print: rect.printProperties
+}
+
+console.log(another.print)
+another.print()
+
+//factory pattern
+
+var createRect = function (width, height) {
+    return {
+        width: width,
+        height: height,
+    
+        draw : function() {
+            console.log('I am a rectAngle')
+        this.printProperties()
+        console.log(this)
+        },
+        printProperties: function() {
+            console.log('My width is' + ' ' + this.width)
+            console.log('My width is' + ' ' + this.height)
+    
+        
+        }
+    }
+}
+
+var rect1 = createRect(10,5)
+rect1.draw()
+
+var rect2 = createRect(40,6)
+rect2.draw()
+
+//const pattern
+
+var Constfactor = function (width, height) {
+        
+            this.width = width
+            this.height = height
+        
+            this.draw = function() {
+                console.log('I am a rectAngle')
+            this.printProperties()
+            console.log(this)
+            },
+            this.printProperties= function() {
+                console.log('My width is' + ' ' + this.width)
+                console.log('My width is' + ' ' + this.height)
+        
+            
+            }
+        }
+    
+
+    var rect3 = new Constfactor(10,5)
+rect1.draw()
+
